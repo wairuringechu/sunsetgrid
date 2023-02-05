2e673d8da896dd0f4d86507dff312c51c53adfd3
Wairuri <ngechmarion@gmail.com>
2023-02-05 10:10:53 +0300

    sunsetgrid

---
+++ myreactapp--use-npm/src/sunsetgrid.js
@@ -0,0 +1,21 @@
+import react from 'react';
+import'./sunsetgrid.css';
+
+const sunsetgrid=()=> {
+    return(
+    <div classname="sunsetgrid">
+            <h1 classname="sunsetphotogrid">best sunset pictures</h1>
+            <div classname="sunset-photos">
+                <img src="https://www.pinterest.com/pin/511721576396854676/"alt="sunset a"/>
+                <img src="https://www.pinterest.com/pin/591519732277685385/"alt="sunset b"/>
+                <img src="https://www.pinterest.com/pin/186899453267406730/"alt="sunset c"/>
+                <img src="https://www.pinterest.com/pin/576953402267460760/"alt="sunset d"/>
+                <img src="https://www.pinterest.com/pin/252483122839632073/"alt="sunset e"/>
+                <img src="https://www.pinterest.com/pin/21321798222355388/"alt="aunset f"/>
+            </div>
+    </div>
+
+    );
+};
+
+export default sunsetgrid;
